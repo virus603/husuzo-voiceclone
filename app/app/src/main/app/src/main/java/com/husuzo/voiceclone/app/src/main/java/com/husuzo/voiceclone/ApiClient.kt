f2fc07eedb734abae333148029567d0c8b77fe25
virus603 <84975565+virus603@users.noreply.github.com>
2025-11-21 03:24:17 +0400

    Create ApiClient.kt

---
+++ app/app/src/main/app/src/main/java/com/husuzo/voiceclone/app/src/main/java/com/husuzo/voiceclone/ApiClient.kt
@@ -0,0 +1,20 @@
+package com.husuzo.voiceclone
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    fun create(baseUrl: String): ApiService {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BASIC
+        val client = OkHttpClient.Builder().addInterceptor(logging).build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+}
