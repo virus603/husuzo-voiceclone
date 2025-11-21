3bd2c637d2a9879bae62e5eceab69e4fa783c5c2
virus603 <84975565+virus603@users.noreply.github.com>
2025-11-21 04:09:02 +0400

    Create ApiService.kt

---
+++ app/src/main/java/com/husuzo/voiceclone/ApiService.kt
@@ -0,0 +1,20 @@
+package com.husuzo.voiceclone
+
+import okhttp3.MultipartBody
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.*
+
+interface ApiService {
+
+    @Multipart
+    @POST("upload_sample")
+    suspend fun uploadSample(
+        @Part file: MultipartBody.Part
+    ): Response<Void>
+
+    @POST("synthesize")
+    suspend fun synthesize(
+        @Body payload: Map<String, String>
+    ): Response<ResponseBody>
+}
